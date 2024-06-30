a99fd8edabf54f2421ee69e56d463e2ea072642a
Elizaveta810 <elizaveta.k810@mail.ru>
2024-06-30 12:20:45 +0300

    борьба со стилями

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 /* import './App.css' */
 
+import Header from "./components/Common/Header";
+
 
 export default function App() {
+  return (
+    <Header/>
+  )
   
   
 }
--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -3,32 +3,19 @@ import "../../css/style.css";
 export default function Header() {
   return (
     <div className="container flex justify-between mx-auto">
-      <div className="flex flex-col space-y-[15px]">
+      <div className="sm: pt-[40px] flex flex-col space-y-[15px]">
         <div>
-          {/* <Link to="/">
-            <img src="" alt="logo">
-          </Link> */}
+          <div>
+            <img src="images/logo.png" alt="logo"/>
+          </div> 
         </div>
         <div>
-          <p className="text-black">Онлайн тренировки для занятий дома//</p>
+          <p className="text-black">Онлайн тренировки для занятий дома</p>
         </div>
       </div>
 
-      <div
-        className="rounded-button bg-mainColor text-black" /* onClick={handleSigninForm} */
-      >
+      <div className="rounded-buttonRadius bg-mainColor text-black rounded hover:mainHover">
         Войти
-      </div>
-      {/* user Блок */}
-      <div>
-        <div>profile_icon</div>
-
-        <div>
-          <div>Сергей</div>
-          <svg>
-           {/*  <use user_arrow_icon="/" /> */}
-          </svg>
-        </div>
       </div>
     </div>
   );
